8012dd98f0d1dd15ea0a8118c704bb25073aa6ea
Amir Gaffar <AmirG7@yahoo.com>
2020-11-24 15:00:43 -0500

    Completed Exercise1

---
+++ DapperDanORM/IDepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DapperDanORM
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments(); //Stubbed out method
+    }
+}
